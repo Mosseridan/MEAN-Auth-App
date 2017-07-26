1bcd929c88a53139c0d3491f8fb1560fa04fe31c
Idan Mosseri <mosseridan@gmail.com>
2017-07-26 20:41:21 +0300

    fiexd db url

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://<dbuser>:<dbpassword>@ds125183.mlab.com:25183/meanauthapp',
+    database: 'mongodb://app:app@ds125183.mlab.com:25183/meanauthapp',
     secret: 'shhhItsAsecret...dontTellAnyone'
 }
